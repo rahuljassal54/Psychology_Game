816b4b6daade5a45af88c8d585e984bd3ae9d89a
Shinzo神 <rahuljassal54rj@gmail.com>
2023-11-16 10:56:52 +0530

    fixed some issues

--- game/Assets/Scripts/Player movement.cs
+++ game/Assets/Scripts/Player movement.cs
@@ -28,7 +28,7 @@ public class BasicPlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        controller.Move(moveDir.normalized * speed * Time.deltaTime); //
     }
 }
 
