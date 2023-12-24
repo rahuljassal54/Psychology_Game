8b6c4a24b596f928f54c947089fe0a2338a155ff
Shinzo神 <rahuljassal54rj@gmail.com>
2023-12-24 16:44:36 +0530

    fixes

--- game/Assets/Scripts/Player movement.cs
+++ game/Assets/Scripts/Player movement.cs
@@ -7,6 +7,11 @@ public class PlayerMovementTutorial : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+    [Header("Slope Movement")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    public float playerHeight;
+
     public Transform orientation;
     float horizontalInput;
     float verticalInput;
@@ -14,15 +19,34 @@ public class PlayerMovementTutorial : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
+    private bool OnSlope(){
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.2f)){
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+    private Vector3 GetSlopeMoveDirection(){
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if(flatVel.magnitude > moveSpeed){
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
 
     private void Update()
     {
         MyInput();
+        SpeedControl();
     }
 
     private void FixedUpdate()
@@ -39,7 +63,12 @@ public class PlayerMovementTutorial : MonoBehaviour
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if(OnSlope()){
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+            Debug.Log("On slope");
+        }
+        else rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.useGravity = !OnSlope();
         anim.SetFloat("isMoving", moveDirection.magnitude);
     }
 
--- game/Assets/Scripts/Third Person Cam.cs
+++ game/Assets/Scripts/Third Person Cam.cs
@@ -19,7 +19,7 @@ public class ThirdPersonCam : MonoBehaviour
     private void FixedUpdate(){
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
-        Debug.Log(viewDir.normalized);
+
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
         if (inputDir != Vector3.zero)
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
